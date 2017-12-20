7f6b0cdd480e95e0a9edb8631f61a755b2dfbf54
Simon Sch <34340168+simisc@users.noreply.github.com>
2017-12-20 14:21:35 +0000

    Initialise repo for splintr package.

---
+++ R/functions.R
@@ -0,0 +1,38 @@
+splintr <- function(..., centre = 0) {
+  n <- splines::ns(...)
+  adj <- splines::predict(n, newx = centre)
+  df <- length(attr(n, "knots")) + 1
+
+  length(adj) == df || stop("Length of adj does not much df.")
+
+  for (i in 1:df) {
+    n[, i] <- n[, i] - adj[i]
+  }
+
+  class(n)[1] <- "splintr"
+  # class(n) <- c("splintr", class(n))
+  attr(n, "centre") <- centre
+  n
+}
+
+makepredictcall.splintr <- function (var, call) {
+  if (as.character(call)[1L] != "splintr")
+    return(call)
+  at <- attributes(var)[c("knots", "Boundary.knots",
+                          "intercept", "centre")]
+  xxx <- call[1L:2L]
+  xxx[names(at)] <- at
+  xxx
+}
+
+predict.splintr <- function (object, newx, ...) {
+  if (missing(newx))
+    return(object)
+  a <- c(list(x = newx),
+         attributes(object)[c("knots",
+                              "Boundary.knots",
+                              "intercept",
+                              "centre")])
+  n <- do.call("splintr", a)
+  # should this be the return value? Check usage n original
+}
