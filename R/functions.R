a0ae3b269639ad249d595e5a1c0358224f087c70
Simon Sch <34340168+simisc@users.noreply.github.com>
2017-12-21 16:01:12 +0000

    Added documentation and tidied up fors and ifs. Requires testing.

--- R/functions.R
+++ R/functions.R
@@ -1,39 +1,43 @@
+#' Generate a basis matrix for natural cubic splines with interpretable intercepts
+#' @export
+#' @param ... Parameters passed to \code{\link[splines]{ns}}.
+#' @param centre The \code{x}-value at which all spline terms should be zero.
+#' @return A matrix of dimension \code{length(x)} by \code{df}, like \code{\link[splines]{ns}}.
+#'   Attributes are that correspond to the arguments to \code{ns()} with an additional
+#'   \code{centre} attribute for use by \code{predict.splintr()}.
 splintr <- function(..., centre = 0) {
   n <- splines::ns(...)
   adj <- predict(n, newx = centre)
-  df <- length(attr(n, "knots")) + 1
-
-  length(adj) == df || stop("Length of adj does not much df.")
-
-  for (i in 1:df) {
-    n[, i] <- n[, i] - adj[i]
-  }
-
-  class(n)[1] <- "splintr"
-  # class(n) <- c("splintr", class(n))
+  # df <- length(attr(n, "knots")) + 1
+  # for (i in 1:df) n[, i] <- n[, i] - adj[i]
+  n <- sweep(n, 2, adj)
+  class(n)[1L] <- "splintr"
   attr(n, "centre") <- centre
-  n
+  return(n)
 }
 
 makepredictcall.splintr <- function (var, call) {
-  if (as.character(call)[1L] != "splintr")
-    return(call)
-  at <- attributes(var)[c("knots", "Boundary.knots",
-                          "intercept", "centre")]
-  xxx <- call[1L:2L]
-  xxx[names(at)] <- at
-  xxx
+  # if (as.character(call)[1L] != "splintr")
+  #   return(call)
+  as.character(call)[1L] != "splintr" && return(call)
+  at <- attributes(var)[c("knots",
+                          "Boundary.knots",
+                          "intercept",
+                          "centre")]
+  x <- call[1L:2L]
+  x[names(at)] <- at
+  return(x)
 }
 
 predict.splintr <- function (object, newx, ...) {
-  if (missing(newx))
-    return(object)
+  # if (missing(newx))
+  #   return(object)
+  missing(newx) && return(object)
   a <- c(list(x = newx),
          attributes(object)[c("knots",
                               "Boundary.knots",
                               "intercept",
                               "centre")])
   n <- do.call("splintr", a)
-  # should this be the return value? Check usage n original
   return(n)
 }
