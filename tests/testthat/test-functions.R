dbf657ef9154a62cb3c34a323c1a92087b283cc8
Simon Schoenbuchner <34340168+simisc@users.noreply.github.com>
2021-09-01 13:13:41 +0100

    added tests

--- tests/testthat/test-functions.R
+++ tests/testthat/test-functions.R
@@ -3,9 +3,18 @@ context("Compare splintr with ns")
 library(splines)
 library(splintr)
 
-m0 <- lm(weight ~ ns(height, df = 5), data = women)
-m1 <- lm(weight ~ splintr(height, df = 5), data = women)
-m2 <- lm(weight ~ splintr(height, df = 5, centre = 67.23), data = women)
+b0 <- ns(women$height, df = 5)
+b1 <- splintr(women$height, df = 5)
+b2 <- splintr(women$height, df = 5, centre = 67.23)
+
+m0 <- lm(women$height ~ b0)
+m1 <- lm(women$height ~ b1)
+m2 <- lm(women$height ~ b2)
+
+test_that("bases are zero at centre", {
+  expect_equal(as.numeric(predict(b1, newx = 0)), rep(0, 5))
+  expect_equal(as.numeric(predict(b2, newx = 67.23)), rep(0, 5))
+})
 
 test_that("models have the same deviance", {
   expect_equal(deviance(m0), deviance(m1))
@@ -13,7 +22,7 @@ test_that("models have the same deviance", {
 })
 
 test_that("models have the same residuals", {
-  expect_equal(resid(m0), resid(m2))
+  expect_equal(resid(m0), resid(m1))
   expect_equal(resid(m0), resid(m2))
 })
 
@@ -21,3 +30,4 @@ test_that("models generate the same predictions", {
   expect_equal(predict(m0), predict(m1))
   expect_equal(predict(m0), predict(m2))
 })
+
