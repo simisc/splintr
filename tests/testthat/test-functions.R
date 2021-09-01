f90e73e92ae754905f1e150c93e609b31caf4436
Simon Schoenbuchner <34340168+simisc@users.noreply.github.com>
2021-09-01 13:00:40 +0100

    added tests

--- tests/testthat/test-functions.R
+++ tests/testthat/test-functions.R
@@ -1,3 +1,23 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+context("Compare splintr with ns")
+
+library(splines)
+library(splintr)
+
+m0 <- lm(weight ~ ns(height, df = 5), data = women)
+m1 <- lm(weight ~ splintr(height, df = 5), data = women)
+m2 <- lm(weight ~ splintr(height, df = 5, centre = 67.23), data = women)
+
+test_that("models have the same deviance", {
+  expect_equal(deviance(m0), deviance(m1))
+  expect_equal(deviance(m0), deviance(m2))
+})
+
+test_that("models have the same residuals", {
+  expect_equal(resid(m0), resid(m2))
+  expect_equal(resid(m0), resid(m2))
+})
+
+test_that("models generate the same predictions", {
+  expect_equal(predict(m0), predict(m1))
+  expect_equal(predict(m0), predict(m2))
 })
