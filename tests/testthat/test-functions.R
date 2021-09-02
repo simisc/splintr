d567d035c32b6a25a45dbfc0a151560b65897107
Simon Schoenbuchner <34340168+simisc@users.noreply.github.com>
2021-09-02 04:45:36 +0100

    added tests

--- tests/testthat/test-functions.R
+++ tests/testthat/test-functions.R
@@ -7,14 +7,14 @@ b0 <- ns(women$height, df = 5)
 b1 <- splintr(women$height, df = 5)
 b2 <- splintr(women$height, df = 5, centre = 67.23)
 
-m0 <- lm(women$height ~ b0)
-m1 <- lm(women$height ~ b1)
-m2 <- lm(women$height ~ b2)
-
 test_that("bases are zero at centre", {
   expect_equal(as.numeric(predict(b1, newx = 0)), rep(0, 5))
   expect_equal(as.numeric(predict(b2, newx = 67.23)), rep(0, 5))
 })
+
+m0 <- lm(weight ~ ns(height, df = 5), data = women)
+m1 <- lm(weight ~ splintr(height, df = 5), data = women)
+m2 <- lm(weight ~ splintr(height, df = 5, centre = 67.23), data = women)
 
 test_that("models have the same deviance", {
   expect_equal(deviance(m0), deviance(m1))
